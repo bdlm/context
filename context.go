ca9c74e3c6df85b2ce4fc18c5db069dc66ca8ec8
Michael Kenney <mkenney@webbedlam.com>
2024-03-22 21:39:08 -0600

    Initial commit

---
+++ context.go
@@ -0,0 +1,31 @@
+package context
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bdlm/log/v2"
+)
+
+// New returns a signal aware root context for services that listens for syscall
+// signals. Additional signals can be passed in.
+func NewContext(additionalSignals ...os.Signal) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Create the application context and start a signal handler.
+	go func() {
+		interrupt := make(chan os.Signal, 1)
+		signal.Notify(
+			interrupt,
+			append(additionalSignals, syscall.SIGTERM)..., // docker, docker-compose, k8s, etc.
+		)
+
+		defer cancel()
+		sig := <-interrupt
+		log.WithField("signal", sig.String()).Info("signal received, shutting down")
+	}()
+
+	return ctx, cancel
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/bdlm/context
+
+go 1.21
+
+require github.com/bdlm/log/v2 v2.0.3
+
+require (
+	github.com/bdlm/errors/v2 v2.0.1 // indirect
+	github.com/bdlm/std/v2 v2.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+	golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a // indirect
+)
